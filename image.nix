14044492d765821a9cd202262098b365eb8b9032
boxboy <30593019+boxboy523@users.noreply.github.com>
2026-09-12 11:26:17 +0900

    Add fixed bash for Docker backend compatibility

--- image.nix
+++ image.nix
@@ -56,6 +56,7 @@ let
       doInstallCheck = false;
     });
 
+  controlBash = pkgs.pkgsStatic.bash;
   controlSuExec = pkgs.pkgsStatic.su-exec;
 in
 pkgs.dockerTools.buildLayeredImage {
@@ -70,6 +71,7 @@ pkgs.dockerTools.buildLayeredImage {
 
   extraCommands = ''
     mkdir -p \
+      bin \
       bootstrap \
       env \
       workspace \
@@ -93,6 +95,7 @@ pkgs.dockerTools.buildLayeredImage {
     install -m644 ${src}/SKILL.md etc/flake-docker/SKILL.md
     install -m644 ${src}/bootstrap-flake.nix bootstrap/flake.nix
 
+    install -m755 ${controlBash}/bin/bash bin/bash
     install -m755 ${controlOpenSSH}/usr/sbin/sshd usr/sbin/sshd
     cp -a ${controlOpenSSH}/usr/lib/ssh/. usr/lib/ssh/
     install -m755 ${controlSuExec}/bin/su-exec sbin/su-exec
